1c9ffe572443640b8d6387601dcc0db2d860bbf6
Angelica <angelica@unet.edu.ve>
2020-06-03 18:30:43 -0300

    Fix de un undefined al agregar el evento de cambio de color en el input

--- app/assets/stylesheets/application.js
+++ app/assets/stylesheets/application.js
@@ -4,26 +4,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
     /**Variables de los inputs de colores*/
     const selectColorPrimario = document.querySelector('#container_color_primario');
     const selectColorSecundario = document.querySelector('#container_color_secundario');
-    let colorPrimario = document.querySelector('#container_color_primario').value;
-    let colorSecundario = document.querySelector('#container_color_secundario').value;
 
-    /**Listener del select de color primario */
-    selectColorPrimario.addEventListener('change', (event) => {
-        
-        if(event.target.value == colorSecundario){
-            alert("Los colores deben ser diferentes")
-            selectColorPrimario.value = colorPrimario;
-        }
-        else colorPrimario = event.target.value; 
-    });
-
-    /**Listener del select de color secundario*/
-    selectColorSecundario.addEventListener('change', (event) => {
-        if(event.target.value == colorPrimario){
-            alert("Los colores deben ser diferentes")
-            selectColorSecundario.value = colorSecundario;
-        }
-        else colorSecundario = event.target.value; 
-    });
+    if(selectColorPrimario && selectColorSecundario){
+        let colorPrimario = selectColorPrimario.value;
+        let colorSecundario = selectColorSecundario.value;
+    
+        /**Listener del select de color primario */
+        selectColorPrimario.addEventListener('change', (event) => {
+            
+            if(event.target.value == colorSecundario){
+                alert("Los colores deben ser diferentes")
+                selectColorPrimario.value = colorPrimario;
+            }
+            else colorPrimario = event.target.value; 
+        });
+    
+        /**Listener del select de color secundario*/
+        selectColorSecundario.addEventListener('change', (event) => {
+            if(event.target.value == colorPrimario){
+                alert("Los colores deben ser diferentes")
+                selectColorSecundario.value = colorSecundario;
+            }
+            else colorSecundario = event.target.value; 
+        });
+    }
+    
     
 });
