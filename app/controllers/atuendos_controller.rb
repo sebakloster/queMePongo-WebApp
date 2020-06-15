65bc53420c8851cca774113202dfcaeda59d3518
Angelica <angelica@unet.edu.ve>
2020-06-15 16:05:39 -0300

    edicion de atuendos, se avanzó con el index de atuendo pero no esta terminado

--- app/controllers/atuendos_controller.rb
+++ app/controllers/atuendos_controller.rb
@@ -1,5 +1,34 @@
 class AtuendosController < ApplicationController
-    
+    def index
+        
+        @atuendos = Atuendo.joins("INNER JOIN prendas ON prendas.id = atuendos.cabeza_id AND prendas.guardarropa_id = 3")
+        logger.info('-antes-');
+        logger.info(@atuendos);
+    end
+
+    def edit
+        @prendas_cabeza = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 1, params[:guardarropa_id])
+        @prendas_torso = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 2, params[:guardarropa_id])
+        @prendas_piernas = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 3, params[:guardarropa_id])
+        @prendas_pies = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 4, params[:guardarropa_id])
+        @atuendo = Atuendo.find(params[:id])
+    end
+
+    def update
+        @atuendo = Atuendo.find(params[:id])
+  
+        if @atuendo.update(atuendos_params)
+            flash[:success]="El atuendo se actualizó correctamente!"
+        
+            @atuendos = Atuendo.joins("INNER JOIN prendas ON prendas.id = atuendos.cabeza_id AND prendas.guardarropa_id = 3")
+        
+            render :index
+        else
+            flash[:error]="El atuendo no se actualizó :("
+            render :edit
+        end
+    end
+
     def new
         @prendas_cabeza = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 1, params[:guardarropa_id])
         @prendas_torso = Prenda.where("prenda_tipo_id = ? AND guardarropa_id = ?", 2, params[:guardarropa_id])
@@ -10,10 +39,10 @@ class AtuendosController < ApplicationController
 
     def show
         @atuendo = Atuendo.find(params[:id])
-      end
+    end
 
     def create
-        logger.info(@params);
+        
         @atuendo = Atuendo.create(atuendos_params);
         if @atuendo.save
             flash[:success]="El atuendo se guardó correctamente!"
@@ -22,6 +51,11 @@ class AtuendosController < ApplicationController
             flash[:error]="El atuendo no se guardó :("
             render :new
         end
+    end
+
+    def destroy
+        Atuendo.destroy(params[:id])
+        redirect_to action: :index
     end
 
     def atuendos_params
